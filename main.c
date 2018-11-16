8d3b44fd07f088634503f64c638137d2b2537a36
Igor Anokhin <igor.anohin@emlid.com>
2018-11-17 01:29:28 +0300

    main: add human readable messages

--- main.c
+++ main.c
@@ -4,13 +4,16 @@
 
 int main(void) {
     image *img = malloc(sizeof(image));
-    printf("Reading result: %d\n", bmp_from_file("input.bmp", img));
+    printf("Reading result: %s\n",
+             bmp_from_file("input.bmp", img) ? "Failed" : "Success");
 
     rotate90(img);
-    printf("Writing result: %d\n", bmp_to_file("output90.bmp", img));
+    printf("Writing result: %s\n", 
+             bmp_to_file("output90.bmp", img) ? "Failed" : "Success");
 
     rotate180(img);
-    printf("Writing result: %d\n", bmp_to_file("output180.bmp", img));
+    printf("Writing result: %s\n", 
+             bmp_to_file("output180.bmp", img) ? "Failed" : "Success");
 
     image_destroy(img);
 }
