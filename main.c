933370c229e337efca2657abecfa23914903c369
Igor Anokhin <igor.anohin@emlid.com>
2018-11-17 01:13:23 +0300

    main: add pretty output

--- main.c
+++ main.c
@@ -4,21 +4,13 @@
 
 int main(void) {
     image *img = malloc(sizeof(image));
-    FILE *f = fopen("input.bmp", "rb");
+    printf("Reading result: %d\n", bmp_from_file("input.bmp", img));
 
-    printf("%d", bmp_from_file("input.bmp", img));
     rotate90(img);
-    if (f)
-        fclose(f);
-    f = fopen("output90.bmp", "wb");
-    printf("%d", bmp_to_file("output90.bmp", img));
-    if (f)
-        fclose(f);
+    printf("Writing result: %d\n", bmp_to_file("output90.bmp", img));
+
     rotate180(img);
-    f = fopen("output180.bmp", "wb");
-    printf("%d", bmp_to_file("output180.bmp", img));
-    if (f)
-        fclose(f);
+    printf("Writing result: %d\n", bmp_to_file("output180.bmp", img));
 
     image_destroy(img);
 }
