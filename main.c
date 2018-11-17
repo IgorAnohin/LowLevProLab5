cb97fe75670791b20bec33e4964b8a3c85878f1c
Igor Anokhin <igor.anohin@emlid.com>
2018-11-17 13:45:17 +0300

    main: fix 180 rotator

--- main.c
+++ main.c
@@ -4,12 +4,15 @@
 
 int main(void) {
     image *img = malloc(sizeof(image));
-    printf("Reading result: %s\n",
+    printf("Reading for 90 result: %s\n",
              bmp_from_file("input.bmp", img) ? "Failed" : "Success");
 
     rotate90(img);
     printf("Writing result: %s\n", 
              bmp_to_file("output90.bmp", img) ? "Failed" : "Success");
+
+    printf("Reading for 180 result: %s\n",
+             bmp_from_file("input.bmp", img) ? "Failed" : "Success");
 
     rotate180(img);
     printf("Writing result: %s\n", 
